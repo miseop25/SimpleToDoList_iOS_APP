c769dfc5607eaa4c312fcb8152647dc726d45fc9
misseop25 <richard25@naver.com>
2020-07-28 00:55:45 +0900

    메인 ToDo 리스트 테이블 뷰 생성

---
+++ SimpleToDoList/SimpleToDoList/ToDoListTableViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ToDoListTableViewController.swift
+//  SimpleToDoList
+//
+//  Created by Minseop Kim on 2020/07/28.
+//  Copyright © 2020 Minseop Kim. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListTableViewController: UIViewController {
+
+    @IBOutlet weak var listTableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
