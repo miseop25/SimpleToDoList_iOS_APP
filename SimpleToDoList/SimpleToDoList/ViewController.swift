10736768398870510ecdc58706cbdc8d47f2f549
misseop25 <richard25@naver.com>
2020-07-27 23:36:47 +0900

    gitignore 생성 및 xcode 프로젝트 생성

---
+++ SimpleToDoList/SimpleToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleToDoList
+//
+//  Created by Minseop Kim on 2020/07/27.
+//  Copyright © 2020 Minseop Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
