5462b6796c687c3d2e098230b9d15565d7dd3dbb
misseop25 <richard25@naver.com>
2020-07-28 22:11:26 +0900

    할일 추가 기능 제작, App 겄다가 킬  경우 데이터가 사라지는 문제 : Database 따로 만들어서 진행해야 할듯

--- SimpleToDoList/SimpleToDoList/ToDoListTableViewController.swift
+++ SimpleToDoList/SimpleToDoList/ToDoListTableViewController.swift
@@ -12,11 +12,61 @@ class ToDoListTableViewController: UIViewController {
 
     @IBOutlet weak var listTableView: UITableView!
     
+    
+    
+    var list = [("할일을 추가하세요", "날짜")]
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+
+    }
+    
+    @IBAction func addTodoList(_ sender: Any) {
+        let alert = UIAlertController(title: "새로운 할일", message: "새로운 할일을 추가해 봐요", preferredStyle: .alert)
+        alert.addTextField { (title) in
+            title.placeholder = "할일"
+        }
+        let addButten = UIAlertAction(title: "추가", style: .default) { (addToDo) in
+            guard let textField = alert.textFields else {return}
+            guard let addToDo = textField.first?.text else {return}
+            
+            let now = Date()
+            let formatter = DateFormatter()
+            formatter.locale = Locale(identifier: "ko_KO")
+            formatter.setLocalizedDateFormatFromTemplate("yyyyMMMMdE")
+            let addTime = formatter.string(from: now)
+            self.list.append((addToDo, addTime))
+            let insertPath = IndexPath(row: self.list.count - 1, section: 0)
+            self.listTableView.beginUpdates()
+            self.listTableView.insertRows(at: [insertPath], with: .automatic)
+            self.listTableView.endUpdates()
+        }
+        let cancleButten = UIAlertAction(title: "취소", style: .cancel, handler: nil)
+        
+        alert.addAction(addButten)
+        alert.addAction(cancleButten)
+        present(alert, animated: true,completion: nil)
+        
     }
     
 
+}
+
+extension ToDoListTableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return list.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = list[indexPath.row].0
+        cell.detailTextLabel?.text = list[indexPath.row].1
+        return cell
+    }
+    
+    
+}
+
+extension ToDoListTableViewController: UITableViewDelegate {
+    
 }
